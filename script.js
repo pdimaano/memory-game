67bf79a4fa8565006fa5f155212259969cfebc90
Philip Dimaano <pdimaano7@gmail.com>
2022-09-17 16:19:55 -0700

    renaming files

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 "use strict";
 
-/** Memory game: find matching pairs of cards and flip both of them. */
-
 const FOUND_MATCH_WAIT_MSECS = 1000;
 const COLORS = [
   "red", "blue", "green", "orange", "purple",
